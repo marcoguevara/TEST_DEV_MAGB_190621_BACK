bb431bdc2b881a07752b88b9d1091176d74547d5
Marco Andres Guevara Bocanegra <marko.alastor@outlook.com>
2021-06-20 11:30:33 -0500

    Se agrega el controlador de personas.

---
+++ Controllers/PersonaFisicaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TEST_DEV_MAGB_190621_BACK.Models;
+using TEST_DEV_MAGB_190621_BACK.Data;
+
+namespace TEST_DEV_MAGB_190621_BACK.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaFisicaController : ControllerBase
+    {
+        private readonly PRUEBASContext _context;
+        public PersonaFisicaController(PRUEBASContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<TbPersonasFisica> Get()
+        {
+            return _context.Set<TbPersonasFisica>().ToArray();
+        }
+    }
+}
